8d2679b52b2fa3cda8167d59e4df73d066f7c99a
Sebastian Sangervasi <villain@harmless.dev>
2020-07-11 18:24:50 -0700

    ts code

---
+++ ts/src/cardScores.test.ts
@@ -0,0 +1,63 @@
+import {
+	parseCsv,
+	calculateScore,
+} from './cardScores'
+const raw = `
+Card Tag,Tag,Result
+agree,agree_good,30
+agree,agree_bad,-10
+agree,,0
+,,
+disagree,disagree_bad,-40
+disagree,,-10
+,,
+listen,listen_good,20
+,,
+butt,butt_good,1000.1
+`
+const exampleTable = [
+	{ cardTag: 'agree', scoreTag: 'agree_good', score: 30 },
+	{ cardTag: 'agree', scoreTag: 'agree_bad', score: -10 },
+	{ cardTag: 'agree', scoreTag: '', score: 0 },
+	{ cardTag: 'disagree', scoreTag: 'disagree_bad', score: -40 },
+	{ cardTag: 'disagree', scoreTag: '', score: -10 },
+	{ cardTag: 'listen', scoreTag: 'listen_good', score: 20 },
+	{ cardTag: 'butt', scoreTag: 'butt_good', score: 1000.1 },
+]
+
+
+describe('calculateScore', () => {
+	test('simple example', () => {
+		const cardTagsStr = 'agree listen'
+		const characterTagsStr = 'listen_good'
+		const nodeTagsStr = 'agree_bad'
+		const result = calculateScore(
+			cardTagsStr,
+			characterTagsStr,
+			nodeTagsStr,
+			exampleTable,
+		)
+		expect(result).toEqual(-10 + 20)
+	})
+
+	test('simple example', () => {
+		const cardTagsStr = 'agree butt'
+		const characterTagsStr = 'agree_good'
+		const nodeTagsStr = 'agree_bad butt_good'
+		const result = calculateScore(
+			cardTagsStr,
+			characterTagsStr,
+			nodeTagsStr,
+			exampleTable,
+		)
+		expect(result).toEqual(-10 + 1000.1)
+	})
+})
+
+describe('parseCsv', () => {
+	test('parseCsv', () => {
+		const result = parseCsv(raw)
+		expect(result).toEqual(exampleTable)
+	})
+
+})
---
+++ ts/src/cardScores.ts
@@ -0,0 +1,81 @@
+type ScoreTable = ScoreRow[]
+interface ScoreRow {
+	cardTag: string
+	scoreTag: string
+	score: number
+}
+
+export const parseCsv = (raw: string): ScoreTable => {
+	const lines = raw.split(/\n+/)
+	const cleanLines = lines
+		.map(line => line.trim())
+		.filter(line => line.length > 0)
+		.filter(line => !line.startsWith('Card'))
+
+	const rows: ScoreRow[] = []
+
+	cleanLines.forEach(line => {
+		const splitted = line.split(',')
+		if (splitted.length !== 3) {
+			throw new Error(`Invalid score row: "${line}"`)
+		}
+		const [cardTag, scoreTag, scoreStr] = splitted
+		if (cardTag === '') {
+			return
+		}
+		const score = Number(scoreStr) || 0
+		rows.push({ cardTag, scoreTag, score })
+	})
+
+	return rows
+}
+
+export const calculateScore = (
+	cardTagsStr: string,
+	characterTagsStr: string,
+	nodeTagsStr: string,
+	scoreTable: ScoreTable,
+): number => {
+	const cardTags = spaceSplit(cardTagsStr)
+	console.log(cardTags)
+
+	const characterTags = spaceSplit(characterTagsStr)
+	const nodeTags = spaceSplit(nodeTagsStr)
+	const effectTags = [...characterTags, ...nodeTags]
+
+	// Build the default map for all card tags
+	const cardTagToEffectTag = new Map<string, string>(
+		cardTags.map(cardTag => [cardTag, '']),
+	)
+	effectTags.forEach(effectTag => {
+		const effectTagPrefix = effectTag.split('_')[0]
+		if (!effectTagPrefix) {
+			console.warn(`ignoring tag "${effectTag}" because it has no prefix`)
+			return
+		}
+		if (cardTagToEffectTag.has(effectTagPrefix)) {
+			cardTagToEffectTag.set(effectTagPrefix, effectTag)
+		}
+	})
+	let scoreSum = 0
+	scoreTable.forEach(({ cardTag, scoreTag, score }) => {
+		if (!cardTagToEffectTag.has(cardTag)) {
+			return
+		}
+		if (cardTagToEffectTag.get(cardTag) === scoreTag) {
+			scoreSum += score
+		}
+	})
+	return scoreSum
+}
+
+const spaceSplit = (str: string): string[] => str.split(/\s+/)
+
+declare var global: {
+	exports: {}
+	ssangervasi?: {
+		parser?: {}
+	}
+}
+global.ssangervasi = global.ssangervasi || {}
+global.ssangervasi.parser = global.ssangervasi.parser || exports
