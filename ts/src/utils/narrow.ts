c0c5d4a8c639ddc6cbc4ca9c623ac490c5d9efa3
Sebastian Sangervasi <villain@harmless.dev>
2021-08-23 15:01:51 -0700

    no debugs

--- ts/src/utils/narrow.ts
+++ ts/src/utils/narrow.ts
@@ -21,36 +21,28 @@ export const some = (...opts: NarrowableArr) => {
 	return opts;
 };
 
-export const narrow = <N extends Narrowable>(
-	n: N,
-	u: unknown,
-	level = 0
-): u is N => {
+export const narrow = <N extends Narrowable>(n: N, u: unknown): u is N => {
 	if (typeof n === "string") {
 		if (n === typeof u) {
 			return true;
 		} else {
-			console.debug(level, `Expected string, received ${typeof u}.`);
 			return false;
 		}
 	}
 
 	if (Array.isArray(n)) {
 		if (SOME in n) {
-			console.debug(level, "narrowing", { n, u });
-			return n.some((t) => narrow(t, u), level + 1);
+			return n.some((t) => narrow(t, u));
 		} else {
 			if (Array.isArray(u)) {
-				return u.every((v) => n.some((t) => narrow(t, v, level + 1)));
+				return u.every((v) => n.some((t) => narrow(t, v)));
 			} else {
-				console.debug(level, `Expected array, received non-array object.`);
 				return false;
 			}
 		}
 	}
 
 	if (typeof u !== "object" || u === null) {
-		console.debug(level, `Expected object, received ${typeof u}.`);
 		return false;
 	}
 
@@ -58,9 +50,8 @@ export const narrow = <N extends Narrowable>(
 
 	return Object.entries(n).every(([k, t]) => {
 		if (k in o) {
-			return narrow(t, o[k], level + 1);
+			return narrow(t, o[k]);
 		} else {
-			console.debug(level, `Expected key "${k}", missing in object.`);
 			return false;
 		}
 	});
