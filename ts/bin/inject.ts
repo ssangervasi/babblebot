f64bad90190ffde0f5d01f2b9f4aea401a42066c
Sebastian Sangervasi <villain@harmless.dev>
2021-10-01 12:49:17 -0700

    update gdr

--- ts/bin/inject.ts
+++ ts/bin/inject.ts
@@ -1,25 +1,93 @@
-// import fs from 'fs'
-// import jsonStringify from 'json-stable-stringify'
-
-import { injectExt, GdProject, refactor } from 'gdevelop-refactor'
+import {
+	injectExt,
+	GdProject,
+	refactor,
+	rewrite,
+	isGdProject,
+	GdResource,
+	isGdExtension,
+	GdExtension,
+	findEvents,
+	GdEvent,
+} from 'gdevelop-refactor'
 
 import { PATHS } from './config'
 
 const main = () => {
-	// injectExt(PATHS.BABBLEBOT)
+	const command = process.argv.slice(-1)[0]
+	if (command === 'code') {
+		injectCode()
+	} else if (command === 'assets') {
+		injectAssets()
+	} else {
+		console.error(`Nothing to do for ${command}`)
+	}
+}
 
-	const res = refactor(
+const injectCode = () => {
+	injectExt(PATHS.BABBLEBOT)
+}
+
+const injectAssets = () => {
+	rewrite(
 		gdProject => {
+			if (!isGdProject(gdProject)) {
+				console.warn('Not a project')
+				return
+			}
+
+			const resourceMap = new Map<string, GdResource>()
 			gdProject.resources.resources.forEach(resource => {
-				console.log(resource.name, resource.file)
+				resourceMap.set(resource.name, resource)
 			})
+
+			console.log(`Project has ${resourceMap.size} resources`)
+
+			rewrite(
+				gdExtension => {
+					if (!isGdExtension(gdExtension)) {
+						console.warn('Not an extension')
+						return
+					}
+
+					gdExtension.eventsFunctions.forEach(eventsFunc => {
+						const finder = findEvents(eventsFunc.events, event => {
+							const dialogueType = 'DialogueTree::LoadDialogueFromJsonFile'
+							type EActions = {
+								actions?: Array<{
+									type: {
+										inverted: boolean
+										value: string
+									}
+									// parameters: ['', 'assets\\encounters\\Amy1\\dialogue.json'],
+									parameters: string[]
+									subInstructions: unknown[]
+								}>
+							}
+							const eventWithActions = event as GdEvent & EActions
+							return (eventWithActions.actions || []).some(
+								a => a.type.value === dialogueType,
+							)
+						})
+						const found = [...finder]
+						if (found.length > 0) {
+							console.log(
+								`${eventsFunc.name}: ${found.length} events with dialogue`,
+							)
+						}
+					})
+				},
+				{
+					inPath: PATHS.BABBLEBOT,
+					readOnly: true,
+				},
+			)
 		},
 		{
 			inPath: PATHS.GAME,
 			readOnly: true,
 		},
 	)
-	console.log(res)
 }
 
 main()
